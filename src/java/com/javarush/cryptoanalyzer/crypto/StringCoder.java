96e686e5d395be4f2ee192e2207f3987772b86b5
AnnVal <anna-valdokhina@yandex.ru>
2022-03-20 15:16:15 +0300

    added class for coding Strings

---
+++ src/java/com/javarush/cryptoanalyzer/crypto/StringCoder.java
@@ -0,0 +1,27 @@
+package com.javarush.cryptoanalyzer.crypto;
+
+public class StringCoder {
+    private int key;
+    private CharCoder charCoder;
+
+    public StringCoder(int key) {
+        this.key = key;
+        this.charCoder = new CharCoder(key);
+    }
+
+    public String encodeString(String string) {
+        char[] chars = string.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = charCoder.encodeChar(chars[i]);
+        }
+        return String.valueOf(chars);
+    }
+
+    public String decodeString(String string) {
+        char[] chars = string.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = charCoder.decodeChar(chars[i]);
+        }
+        return String.valueOf(chars);
+    }
+}
